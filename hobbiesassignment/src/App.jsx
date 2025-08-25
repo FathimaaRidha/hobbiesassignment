20554b74ad603d741ffc319b41528ccb5afc9328
FathimaaRidha <fathimaridha.cs@gmail.com>
2025-08-25 15:05:06 +0530

    first commit

---
+++ hobbiesassignment/src/App.jsx
@@ -0,0 +1,27 @@
+import Card from './components/Card'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Card
+        title="Card Title"
+        description="This is a description for the card."
+        imageUrl="https://www.google.com/imgres?q=coding%20image&imgurl=https%3A%2F%2Fimages.pexels.com%2Fphotos%2F943096%2Fpexels-photo-943096.jpeg%3Fcs%3Dsrgb%26dl%3Dpexels-danny-meneses-340146-943096.jpg%26fm%3Djpg&imgrefurl=https%3A%2F%2Fwww.pexels.com%2Fsearch%2Fcoding%2F&docid=vLxISuumaP92rM&tbnid=QFsSTYbZOYfsSM&vet=12ahUKEwjx8J_30KWPAxXGR2wGHfcSATIQM3oECBQQAA..i&w=6000&h=4000&hcb=2&ved=2ahUKEwjx8J_30KWPAxXGR2wGHfcSATIQM3oECBQQAA"
+      />
+      <Card  
+        title="Card Title"
+        description="This is a description for the card."
+        imageUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSIfls1MosRYemhUCbTpuhmoc8eoAVVb-iDhA&s"
+        />
+      <Card
+        title="Card Title"
+        description="This is a description for the card."
+        imageUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTWKWbS51AVnS-Qy3mquBPl5Vq3N20WsmAxQg&s"
+      />
+    </>
+  )
+}
+
+export default App
